fa310152838328248bb1c9f6060ace8644b6cbc3
Grant Collins <grantcol@usc.edu>
2015-02-28 02:52:06 -0800

    stubbed all core classes

--- php/core.php
+++ php/core.php
@@ -2,21 +2,21 @@
 
 class Artist
 {
-	public $name;
-	public $spId;
-	public $imgUrls;
-	public $genre;
-	public $songs;
+	public $mName;
+	public $mSpId;
+	public $mImgUrls;
+	public $mGenre;
+	public $mSongs;
 
-	function __construct($n, $id, $img, $g, $s) {
-		$name = $n;
-		$spId = $id;
-		$imgUrls = $img;
-		$genre = $g;
-		$songs = $s;
+	function __construct($name, $id, $img, $genre, $songs) {
+		$mName = $name;
+		$mSpId = $id;
+		$mImgUrls = $img;
+		$mGenre = $genre;
+		$mSongs = $songs;
 	}
 
-	function setSongs($songArr) {
+	public function setSongs($songArr) {
 		$songs = $songArr;
 	}
 
@@ -24,15 +24,33 @@ class Artist
 
 class Song
 {
-	public $name;
-	public $album;
-	public $artist;
-	public $freq;
+	public $mName;
+	public $mAlbum;
+	public $mArtist;
+	public $mLyrics;
+	public $mWords;
+
+	public function parseLyrics() {}
 }
 
 class Album 
 {
-	public $title;
-	public $artist;
+	public $mTitle;
+	public $mArtist;
+	public $mCoverImg;
+}
+
+class Word 
+{
+	public $mWord;
+	public $mSize;
+	public $mFreq;
+	public $mSong;
+}
+
+class Cloud
+{
+	public $mArtist;
+	public $mLyrics;
 }
 ?>
