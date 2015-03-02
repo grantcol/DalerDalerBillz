c3caabb8141713f8e51c1258746bb2f7148050ec
Daler <asrorids@gmail.com>
2015-03-01 19:57:55 -0800

    Saved the changes that work and added CSS style to it

--- php/core.php
+++ php/core.php
@@ -1,5 +1,4 @@
 <?php 
-
 class Artist
 {
 	public $mName;
@@ -8,16 +7,17 @@ class Artist
 	public $mGenre;
 	public $mSongs;
 
-	function __construct($name, $id, $img, $genre, $songs) {
-		$mName = $name;
-		$mSpId = $id;
-		$mImgUrls = $img;
-		$mGenre = $genre;
-		$mSongs = $songs;
+	function __construct() {
+		$mName = "";//$name;
+		$mSpId = "";//$id;
+		$mImgUrls = "";//$img;
+		$mGenre = "";//$genre;
+		$mSongs = "";//$songs;
 	}
 
 	public function setSongs($songArr) {
-		$songs = $songArr;
+		
+		$this->mSongs = $songArr;
 	}
 
 }
@@ -30,18 +30,40 @@ class Song
 	public $mLyrics;
 	public $mWords;
 
-	private $mBlackList = array('fuck', 'shit', 'ass', 'bitch', 'goddamn');
+	private $mBlackList = array(
+									'fuck', 'shit', 'ass', 'bitch', 'damn', 'nigga',
+									'I', 'as', 'a', 'the'
+								);
+
+	public function __construct($name, $artist) {
+		$this->mName = $name;
+		$this->mArtist = $artist;
+	}
+
+	public function setLyrics($lyrics) {
+		$this->mLyrics = $lyrics;
+	}
+
 	public function parseLyrics() {
-		$words = array();
-		foreach($mLyrics as $l){
-			if(!in_array($l, $mBlackList)) {
-				$words[$l];
-			} 
-			else {
-				str_replace();
+		$fileContents = file_get_contents("../tests/legend.txt");
+		if($fileContents != FALSE) {
+			$this->mLyrics = explode(" ", $fileContents);
+			foreach($this->mLyrics as $l) {
+				if(!in_array($l, $this->mBlackList)) {
+					$this->mWords[$l] += 1;
+				}
 			}
-			
+			//var_dump($this->mWords);
 		}
+		else {
+			echo "failed";
+			$this->mLyrics = NULL;
+			$this->mWords  = NULL;
+		}
+	}
+
+	public function getFrequency($word) {
+		return $mWords[$word];
 	}
 }
 
@@ -63,6 +85,75 @@ class Word
 class Cloud
 {
 	public $mArtist;
-	public $mLyrics;
+	public $mWords;
+
+	public function __construct($artist) {
+		$this->mArtist = $artist;
+		$this->generateCloud();
+	}
+	//Loop through mArtist's list of songs and find the most frequent words
+	public function findMostFrequent() {
+		$words = $this->mArtist->mSongs->mWords;
+		$sorted = arsort($words);
+		if( $sorted ) {
+			//arr sorted by value choose top 
+			if( count($words) > 250 ) {
+				//slice the array to be 250 or less
+				array_slice($words, 0, 250);
+			}
+			$this->mWords = $words;
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	public function generateCloud() {
+		if($this->findMostFrequent()) {
+			//$mWords is now sorted and cleared for use
+			//Loop through each pair and find out the freq
+			//Choose a size falue based on the freq of the word
+			//For now, until css is complete just add the word at standard size
+			foreach( $this->mWords as $word => $freq ) {
+				//echo $this->calcSize($freq);
+				echo "<span class='dummy' style='font-size: ".$this->calcSize($freq).";'><a href='#'>".$word."</a></span>";
+			}
+		}
+		else {
+			echo "failed";
+		}
+	}
+
+	public function calcSize( $wordFreq ) {
+		$minSize = 10;
+		$midSize = 15;
+		$maxSize = 25;
+
+		if($wordFreq < 5){
+			return "cloud-word-big";
+		}
+		else if($wordFreq > 5 && $wordFreq < 10) {
+			return "cloud-word-medium";
+		}
+		else {
+			return "cloud-word-small";
+		}
+
+	}
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title> LyricsCloud </title>
+<link rel="stylesheet" href="lyricscloud.css">
+</head>
+	<body>
+		<?php 
+			$testSong = new Song();
+			$testSong->parseLyrics();
+			$testArtist = new Artist();
+			$testArtist->setSongs($testSong);
+			$testCloud = new Cloud($testArtist);
+		?>
+	</body>
+</html>
